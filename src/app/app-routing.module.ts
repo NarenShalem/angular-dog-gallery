140489d84d6d4ef69c266f23a4fe49eab0a90b69
mnarendrab <narendra.m@knowledgehut.co>
2020-08-24 18:05:57 +0530

    Angular setup done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'dogs',
+    loadChildren: () => import('./dogs/dogs.module').then((m) => m.DogsModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
